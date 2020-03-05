3c3a55d7654d9263abdbd0249ccf5caf8c218eed
John Curry <cjohn98@vt.edu>
2020-03-05 12:18:40 -0500

    close to done

--- homework3.c
+++ homework3.c
@@ -35,6 +35,7 @@ int main(void)
 
         // TODO: If Timer0 has expired, increment count0.
         // YOU MUST WRITE timer0expired IN myTimer.c
+
         if (timer0Expired())
         {
             count0 = ((count0 + 1) % 8);
@@ -55,7 +56,7 @@ int main(void)
 
         if (check)
         {
-            count1 = (count1 + 1) % 8;
+            count1 = ((count1 + 1) % 8);
         }
 
     }
@@ -132,45 +133,45 @@ void changeBoosterpackLED(unsigned int count1)
     {
 
     case 1: //state 1 red
-        //callred LED
-        turnOn_BoosterpackLEDRed();
+        //off
+        turnOff_BoosterpackLEDRed();
         turnOff_BoosterpackLEDBlue();
         turnOff_BoosterpackLEDGreen();
         break;
-    case 2: // green
+    case 2: // red
+        turnOn_BoosterpackLEDRed();
+        turnOff_BoosterpackLEDGreen();
+        turnOff_BoosterpackLEDBlue();
+        break;
+    case 3:  //green
         turnOff_BoosterpackLEDRed();
         turnOn_BoosterpackLEDGreen();
         turnOff_BoosterpackLEDBlue();
         break;
-    case 3:  //yellow
+    case 4:  //  yellow
         turnOn_BoosterpackLEDRed();
         turnOn_BoosterpackLEDGreen();
         turnOff_BoosterpackLEDBlue();
         break;
-    case 4:  // blue
+    case 5://blue
         turnOff_BoosterpackLEDRed();
         turnOff_BoosterpackLEDGreen();
         turnOn_BoosterpackLEDBlue();
         break;
-    case 5:  //pink
+    case 6:  //ppink
         turnOn_BoosterpackLEDRed();
-        turnOff_BoosterpackLEDGreen();
-        turnOn_BoosterpackLEDBlue();
-        break;
-    case 6:   //light blue
-        turnOff_BoosterpackLEDRed();
-        turnOn_BoosterpackLEDGreen();
-        turnOn_BoosterpackLEDBlue();
-        break;
-    case 7: //white
-        turnOn_BoosterpackLEDRed();
-        turnOn_BoosterpackLEDGreen();
-        turnOn_BoosterpackLEDBlue();
-        break;
-    case 8:  //off
-        turnOff_BoosterpackLEDRed();
         turnOff_BoosterpackLEDGreen();
         turnOff_BoosterpackLEDBlue();
+        break;
+    case 7: //light blue
+        turnOff_BoosterpackLEDRed();
+        turnOn_BoosterpackLEDGreen();
+        turnOn_BoosterpackLEDBlue();
+        break;
+    case 8:  //white
+        turnOn_BoosterpackLEDRed();
+        turnOn_BoosterpackLEDGreen();
+        turnOn_BoosterpackLEDBlue();
         break;
     }
 }
@@ -190,14 +191,17 @@ bool fsmBoosterpackButtonS1(unsigned char buttonhistory) //what is buttonhistory
     // in summary part of slides
 
     bool pressed = false;
+
     static enum
     {
         up, down
-    } state = up;
+    }
+
+    state = up;
     switch (state)
     {
     case up:
-        if (buttonhistory == 0xff) //why not 0 mess with this later
+        if (buttonhistory == 0xff) //why doesn't it work w/1 mess with this later
         {
             state = down;
             pressed = true;
--- myGPIO.c
+++ myGPIO.c
@@ -259,8 +259,9 @@ void turnOn_BoosterpackLEDRed()
 // TODO: Create a function to turn off the Red Boosterpack LED2.
 void turnOff_BoosterpackLEDRed()
 {
-    P2OUT |= boosterRGB_RED;
-    P2OUT = P2OUT &~boosterRGB_RED;
+   // P2OUT |= boosterRGB_RED;
+    //P2OUT = P2OUT &~boosterRGB_RED;
+    P2OUT &= boosterRGB_RED;
 }
 
 // TODO: Create a function to turn on the Green Boosterpack LED2.
@@ -273,8 +274,9 @@ void turnOn_BoosterpackLEDGreen()
 // TODO: Create a function to turn off the Green Boosterpack LED2.
 void turnOff_BoosterpackLEDGreen()
 {
-    P2OUT |= boosterRGB_GREEN;
-    P2OUT = P2OUT &~boosterRGB_GREEN;
+//    P2OUT |= boosterRGB_GREEN;
+//    P2OUT = P2OUT &~boosterRGB_GREEN;
+    P2OUT &= boosterRGB_GREEN;
 }
 
 // TODO: Create a function to turn on the Blue Boosterpack LED2.
@@ -287,6 +289,6 @@ void turnOn_BoosterpackLEDBlue()
 // TODO: Create a function to turn off the Green Boosterpack LED2.
 void turnOff_BoosterpackLEDBlue()
 {
-    P5OUT |= boosterRGB_BLUE;
-    P5OUT = P5OUT &~boosterRGB_BLUE;
+
+    P5OUT &= boosterRGB_BLUE;
 }
